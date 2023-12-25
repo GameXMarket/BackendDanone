21104fd8f5a42cf4da499b9ac7a16d1d9bbd26d5
yunikeil <iyunakov@inbox.ru>
2023-12-25 23:02:16 +0000

    token receipt schemes have been changed, explicit error handling has been added

--- core/depends/depends.py
+++ core/depends/depends.py
@@ -1,6 +1,6 @@
 from typing import Generator, Tuple, Any
 
-from fastapi.security import APIKeyCookie
+from fastapi.security import APIKeyCookie, APIKeyHeader, OAuth2PasswordBearer
 from fastapi import Request, HTTPException, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -14,24 +14,38 @@ from core.database import get_session
 from core import settings as conf
 
 
-# ! В случае надобности, просто меняем схему получения токенов
-access_cookie_scheme = APIKeyCookie(
-    name="access",
-    scheme_name="Cookie access token",
-    description="Поле и кнопка ниже ни на что не влияют, они сделаны для отображения запросов, требующих авторизации",
-)
+if conf.DEBUG:
+    access_token_scheme = APIKeyCookie(
+        name="access",
+        scheme_name="Cookie access token",
+        description="Поле и кнопка ниже ни на что не влияют, они сделаны для отображения запросов, требующих авторизации",
+        auto_error=False,
+    )
+else:
+    access_token_scheme = APIKeyHeader(
+        name="Authorization",
+        scheme_name="Header Authorization token",
+        description="Поле и кнопка ниже ни на что не влияют, они сделаны для отображения запросов, требующих авторизации",
+        auto_error=False,
+    )
 
-refresh__cookie_scheme = APIKeyCookie(
+refresh_token_scheme = APIKeyCookie(
     name="refresh",
     scheme_name="Cookie refresh token",
     description="Поле и кнопка ниже ни на что не влияют, они сделаны для отображения запросов, требующих авторизации",
+    auto_error=False,
 )
 
 
 async def get_refresh(
-    refresh_t=Depends(refresh__cookie_scheme),
+    refresh_t=Depends(refresh_token_scheme),
     db_session: AsyncSession = Depends(get_session),
 ) -> schemas_t.JwtPayload:
+    if not refresh_t:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="Not authenticated"
+        )
+
     token_data: schemas_t.JwtPayload = await TokenSecurity.verify_jwt_token(
         token=refresh_t, secret=conf.REFRESH_SECRET_KEY, db_session=db_session
     )
@@ -50,9 +64,22 @@ async def get_refresh(
 
 
 async def get_access(
-    access_t=Depends(access_cookie_scheme),
+    access_t: str = Depends(access_token_scheme),
     db_session: AsyncSession = Depends(get_session),
 ) -> schemas_t.JwtPayload:
+    if not access_t:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="Not authenticated"
+        )
+
+    if isinstance(access_token_scheme, APIKeyHeader):
+        if not access_t.startswith("Bearer "):
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Could not validate header credentials",
+            )
+        access_t = access_t.replace("Bearer ", "")
+
     token_data = await TokenSecurity.verify_jwt_token(
         token=access_t, secret=conf.ACCESS_SECRET_KEY, db_session=db_session
     )
@@ -65,11 +92,10 @@ async def get_access(
 
     return token_data
 
-# TODO Others
 
 async def auto_token_ban(
-    refresh_t=Depends(refresh__cookie_scheme),
-    access_t=Depends(access_cookie_scheme),
+    refresh_t=Depends(refresh_token_scheme),
+    access_t=Depends(access_token_scheme),
     db_session: AsyncSession = Depends(get_session),
 ) -> None:
     refresh_data = await TokenSecurity.verify_jwt_token(
@@ -159,17 +185,19 @@ def get_current_admin_user(
     return __check_user_role(3, current_active_user)
 
 
-__base_responses: dict  = {
+__base_responses: dict = {
     get_refresh: {
         "parrent": None,
-        401: {"model": schemas_u.UserError}
+        403: {"model": schemas_t.TokenError},
+        401: {"model": schemas_u.UserError},
     },
     get_access: {
         "parrent": None,
-        401: {"model": schemas_u.UserError}
+        403: {"model": schemas_t.TokenError},
+        401: {"model": schemas_u.UserError},
     },
     auto_token_ban: {
-        "parrent": None,                 
+        "parrent": None,
     },
     get_current_user: {
         "parrent": get_access,
@@ -177,24 +205,24 @@ __base_responses: dict  = {
     },
     get_current_active_user: {
         "parrent": get_current_user,
-        404: {"model": schemas_u.UserError}
+        404: {"model": schemas_u.UserError},
     },
-    get_current_user_user : {
+    get_current_user_user: {
         "parrent": get_current_active_user,
-        401: {"model": schemas_u.UserError}
+        401: {"model": schemas_u.UserError},
     },
-    get_current_mod_user : {
+    get_current_mod_user: {
         "parrent": get_current_active_user,
-        401: {"model": schemas_u.UserError}
+        401: {"model": schemas_u.UserError},
     },
-    get_current_arbit_user : {
+    get_current_arbit_user: {
         "parrent": get_current_active_user,
-        401: {"model": schemas_u.UserError}
+        401: {"model": schemas_u.UserError},
     },
-    get_current_admin_user : {
+    get_current_admin_user: {
         "parrent": get_current_active_user,
-        401: {"model": schemas_u.UserError}
-    }
+        401: {"model": schemas_u.UserError},
+    },
 }
 
 
@@ -205,13 +233,11 @@ def build_response(func: object, base_responses: dict = __base_responses):
     while response_dict:
         final_response.update(response_dict)
         parent_func = response_dict.get("parrent")
-        
+
         if parent_func:
             response_dict = base_responses.get(parent_func, {})
         else:
             break
-    
+
     del final_response["parrent"]
     return final_response
-    
-
