90fdc086c250b4494fdb6b47f68b83f28b1c6701
yunikeil <iyunakov@inbox.ru>
2024-01-13 08:33:00 +0300

    base folders created

---
+++ app/messages/__init__.py
@@ -0,0 +1,10 @@
+exit(1)
+
+from fastapi import APIRouter
+
+from .routers import router_category_carcass, router_category_value
+
+
+category_routers = APIRouter(prefix="/categories", tags=["categories"])
+category_routers.include_router(router_category_carcass, prefix="/carcass")
+category_routers.include_router(router_category_value, prefix="/value")
