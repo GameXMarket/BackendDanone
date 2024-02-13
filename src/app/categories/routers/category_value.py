248a15a17b9aa95d53c280e474f5abd346fce215
iyunakov <iyunakov@inbox.ru>
2024-02-13 22:15:52 +0300

    added get_value and base for get_associated_values

--- src/app/categories/routers/category_value.py
+++ src/app/categories/routers/category_value.py
@@ -17,10 +17,39 @@ router = APIRouter()
 base_session = deps.UserSession()
 
 
-@router.post(
-    path="/{carcass_id}"
-)
-async def create_value(carcass_id: int, value: schemas.ValueCreate, current_session: tuple[schemas_t.JwtPayload ,deps.UserSession] = Depends(base_session), db_session: AsyncSession = Depends(get_session)):
+@router.get(path="/getassociated")
+async def get_associated_values(root_value_id: int, db_session: AsyncSession = Depends(get_session)):
+    """
+    root_value_id - id value, по которому будет поиск всех связанных value
+    метод ещё не готов
+    """
+    values = await services.categories_values.get_associated_by_id(db_session, root_value_id)
+    
+    if not values:
+        raise HTTPException(status_code=404)
+    
+    return values
+
+
+@router.get(path="/{value_id}")
+async def get_value(value_id: int, db_session: AsyncSession = Depends(get_session)):
+    value = await services.categories_values.get_by_id(db_session, id=value_id)
+    
+    if not value:
+        raise HTTPException(status_code=404)
+    
+    return value
+
+
+@router.post(path="/{carcass_id}")
+async def create_value(
+    carcass_id: int,
+    value: schemas.ValueCreate,
+    current_session: tuple[schemas_t.JwtPayload, deps.UserSession] = Depends(
+        base_session
+    ),
+    db_session: AsyncSession = Depends(get_session),
+):
     """
     Создаёт value у каркасса
     """
@@ -30,15 +59,22 @@ async def create_value(carcass_id: int, value: schemas.ValueCreate, current_sess
     if not user.is_admin():
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
 
-    value = await services.categories_values.create_value(db_session, author_id=user.id, carcass_id=carcass_id, value=value)
-    
+    value = await services.categories_values.create_value(
+        db_session, author_id=user.id, carcass_id=carcass_id, value=value
+    )
+
     return value
 
 
-@router.put(
-    path="/{value_id}"
-)
-async def update_value(value_id: int, new_value: schemas.ValueUpdate, current_session: tuple[schemas_t.JwtPayload ,deps.UserSession] = Depends(base_session), db_session: AsyncSession = Depends(get_session)):
+@router.put(path="/{value_id}")
+async def update_value(
+    value_id: int,
+    new_value: schemas.ValueUpdate,
+    current_session: tuple[schemas_t.JwtPayload, deps.UserSession] = Depends(
+        base_session
+    ),
+    db_session: AsyncSession = Depends(get_session),
+):
     """
     Обновляет value по его id
     """
@@ -49,20 +85,25 @@ async def update_value(value_id: int, new_value: schemas.ValueUpdate, current_se
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
 
     value = await services.categories_values.get_by_id(db_session, id=value_id)
-    
+
     if not value:
         raise HTTPException(status_code=404)
-    
-    new_value = await services.categories_values.update_value(db_session, author_id=user.id, db_obj=value, value=new_value)
-    
+
+    new_value = await services.categories_values.update_value(
+        db_session, author_id=user.id, db_obj=value, value=new_value
+    )
+
     return new_value
 
 
-
-@router.delete(
-    path="/{value_id}"
-)
-async def delete_value(value_id: int, current_session: tuple[schemas_t.JwtPayload ,deps.UserSession] = Depends(base_session), db_session: AsyncSession = Depends(get_session)):
+@router.delete(path="/{value_id}")
+async def delete_value(
+    value_id: int,
+    current_session: tuple[schemas_t.JwtPayload, deps.UserSession] = Depends(
+        base_session
+    ),
+    db_session: AsyncSession = Depends(get_session),
+):
     """
     Удаляет value по его id
     """
@@ -71,11 +112,12 @@ async def delete_value(value_id: int, current_session: tuple[schemas_t.JwtPayloa
 
     if not user.is_admin():
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
-    
-    deleted_value = await services.categories_values.delete_value(db_session, value_id=value_id)
+
+    deleted_value = await services.categories_values.delete_value(
+        db_session, value_id=value_id
+    )
 
     if not deleted_value:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
-    
-    return deleted_value
 
+    return deleted_value
--- src/app/categories/services/categories_values.py
+++ src/app/categories/services/categories_values.py
@@ -3,12 +3,15 @@ from typing import Tuple, List, Any
 
 from fastapi.encoders import jsonable_encoder
 from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import selectinload
 from sqlalchemy import select, update, exists, delete, and_
 
 from .. import models, schemas
 
 
-async def get_by_id(db_session: AsyncSession, *, id: int, options: List[Tuple[Any]] = None) -> models.CategoryValue | None:
+async def get_by_id(
+    db_session: AsyncSession, id: int, options: List[Tuple[Any]] = None
+) -> models.CategoryValue | None:
     stmt = select(models.CategoryValue).where(models.CategoryValue.id == id)
     if options:
         for option in options:
@@ -17,12 +20,23 @@ async def get_by_id(db_session: AsyncSession, *, id: int, options: List[Tuple[An
     return value
 
 
-async def get_by_carcass_id(db_session: AsyncSession, *, carcass_id: int, options: List[Tuple[Any]] = None) -> List[models.CategoryValue] | List[None]:
-    stmt = select(models.CategoryValue).where(models.CategoryValue.carcass_id == carcass_id)
+async def get_associated_by_id(
+    db_session: AsyncSession, root_id: int, options: List[Tuple[Any]] = None, v_ids: set = set()
+):
+    ...    
+
+async def get_by_carcass_id(
+    db_session: AsyncSession, *, carcass_id: int, options: List[Tuple[Any]] = None
+) -> List[models.CategoryValue] | List[None]:
+    stmt = select(models.CategoryValue).where(
+        models.CategoryValue.carcass_id == carcass_id
+    )
     if options:
         for option in options:
             stmt = stmt.options(option[0](option[1]))
-    values: List[models.CategoryValue] | List[None] = (await db_session.execute(stmt)).scalars()
+    values: List[models.CategoryValue] | List[None] = (
+        await db_session.execute(stmt)
+    ).scalars()
     return values
 
 
@@ -64,7 +78,9 @@ async def update_value(
     return db_obj
 
 
-async def delete_value(db_session: AsyncSession, *, value_id: int) -> models.CategoryValue:
+async def delete_value(
+    db_session: AsyncSession, *, value_id: int
+) -> models.CategoryValue:
     value = await get_by_id(db_session, id=value_id)
 
     if not value:
@@ -74,14 +90,3 @@ async def delete_value(db_session: AsyncSession, *, value_id: int) -> models.Cat
     await db_session.commit()
 
     return value
-
-
-
-
-
-
-
-
-
-
-
