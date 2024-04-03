3750a861d58da9ff3c2a9ff58f86aec08a96611e
iyunakov <iyunakov@inbox.ru>
2024-04-03 17:43:12 +0300

    Revert irrelevant to this branch changes
    This reverts commits:  4a5aa652b113e42696ba8fd0b2ff4983dc218c87
    a8c4c5bf3b1413292cc4fc5cce9519cedc5ebefc

--- src/core/tests/conftest.py
+++ src/core/tests/conftest.py
@@ -1,15 +1,18 @@
+from typing import Any, AsyncGenerator
 import pytest
 from httpx import AsyncClient
 
 from main import app
 
 
-@pytest.fixture(scope="session")
+@pytest.fixture
 def anyio_backend():
     return "asyncio"
 
 
-@pytest.fixture(scope="session")
-async def async_client():
+@pytest.fixture
+async def async_client() -> AsyncGenerator[AsyncClient, Any, None]:
     async with AsyncClient(app=app, base_url="http://testserver") as async_client:
         yield async_client 
+
+
