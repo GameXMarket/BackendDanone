455c860f8aa7c9fe3354aab4beeefa1972ca6970
Kropka <jugerror@gmail.com>
2024-04-06 19:24:27 +0300

    DRY ENABLED 😎 Added new categories

--- src/core/database/preload_data.py
+++ src/core/database/preload_data.py
@@ -31,256 +31,148 @@ async def __init_categories():
 
 
 async def __init_categories_carcass():
+    categories_to_create = [
+        {
+            "select_name": "Выберите игру",
+            "is_root": True,
+            "in_offer_name": "Игра",
+            "admin_comment": "Выбор игры",
+            "is_last": False,
+        },
+        {
+            "select_name": "Выберите услугу",
+            "is_root": False,
+            "in_offer_name": "Тип объявления",
+            "admin_comment": "Услуги для игры",
+            "is_last": False,
+        },
+        {
+            "select_name": "Выберите уровни БП",
+            "is_root": False,
+            "in_offer_name": "Уровни БП",
+            "admin_comment": "Уровни боевого пропуска для BrawlStars",
+            "is_last": True,
+        },
+        {
+            "select_name": "Выберите способ доставки",
+            "is_root": False,
+            "in_offer_name": "Способ доставки",
+            "admin_comment": "Способ доставки для игры",
+            "is_last": False,
+        },
+        {
+            "select_name": "Выберите номинал",
+            "is_root": False,
+            "in_offer_name": "Номинал",
+            "admin_comment": "Номинал гемов для игры BrawlStars",
+            "is_last": True,
+        },
+        {
+            "select_name": "Выберите количество ММР",
+            "is_root": False,
+            "in_offer_name": "Количество ММР",
+            "admin_comment": "Количество ММР для игры Dota 2",
+            "is_last": True,
+        },
+        {
+            "select_name": "Выберите количество ELO",
+            "is_root": False,
+            "in_offer_name": "Количество ELO",
+            "admin_comment": "Количество ELO для игры CS2",
+            "is_last": True,
+        },
+    ]
     async with context_get_session() as db_session:
         carcass = await services_c.categories_carcass.get_all(db_seesion=db_session)
         if not carcass or conf.DROP_TABLES:
-            carcass: models_c.category_carcasses.CategoryCarcass = (
+            for category_data in categories_to_create:
                 await services_c.categories_carcass.create_category(
                     db_session=db_session,
                     author_id=1,
-                    obj_in=schemas_c.CategoryCarcassCreate(
-                        select_name="Выберите игру",
-                        is_root=True,
-                        in_offer_name="Игра",
-                        admin_comment="Выбор игры",
-                        is_last=False,
-                    ),
+                    obj_in=schemas_c.CategoryCarcassCreate(**category_data),
                 )
-            )
-            carcass: models_c.category_carcasses.CategoryCarcass = (
-                await services_c.categories_carcass.create_category(
-                    db_session=db_session,
-                    author_id=1,
-                    obj_in=schemas_c.CategoryCarcassCreate(
-                        select_name="Выберите услугу",
-                        is_root=False,
-                        in_offer_name="Тип объявления",
-                        admin_comment="Услуги для игры",
-                        is_last=False,
-                    ),
-                )
-            )
-            carcass: models_c.category_carcasses.CategoryCarcass = (
-                await services_c.categories_carcass.create_category(
-                    db_session=db_session,
-                    author_id=1,
-                    obj_in=schemas_c.CategoryCarcassCreate(
-                        select_name="Выберите уровни БП",
-                        is_root=False,
-                        in_offer_name="Уровни БП",
-                        admin_comment="Уровни боевого пропуска для BrawlStars",
-                        is_last=True,
-                    ),
-                )
-            )
-            carcass: models_c.category_carcasses.CategoryCarcass = (
-                await services_c.categories_carcass.create_category(
-                    db_session=db_session,
-                    author_id=1,
-                    obj_in=schemas_c.CategoryCarcassCreate(
-                        select_name="Выберите способ доставки",
-                        is_root=False,
-                        in_offer_name="Способ доставки",
-                        admin_comment="Способ доставки для игры BrawlStars",
-                        is_last=False,
-                    ),
-                )
-            )
-            carcass: models_c.category_carcasses.CategoryCarcass = (
-                await services_c.categories_carcass.create_category(
-                    db_session=db_session,
-                    author_id=1,
-                    obj_in=schemas_c.CategoryCarcassCreate(
-                        select_name="Выберите номинал",
-                        is_root=False,
-                        in_offer_name="Номинал",
-                        admin_comment="Номинал гемов для игры BrawlStart",
-                        is_last=True,
-                    ),
-                )
-            )
 
 
 async def __init_categories_values():
+    values_to_create = [
+        {"carcass_id": 1, "value": "Counter-Strike 2", "next_carcass_id": 2},
+        {"carcass_id": 2, "value": "Буст ELO)", "next_carcass_id": 7},
+        {"carcass_id": 7, "value": "25 ELO", "next_carcass_id": None},
+        {"carcass_id": 7, "value": "50 ELO", "next_carcass_id": None},
+        {"carcass_id": 7, "value": "100 ELO", "next_carcass_id": None},
+        {"carcass_id": 7, "value": "200 ELO", "next_carcass_id": None},
+
+        {"carcass_id": 1, "value": "Dota 2", "next_carcass_id": 2},
+        {"carcass_id": 2, "value": "Буст эмэмэрау)", "next_carcass_id": 6},
+        {"carcass_id": 6, "value": "100 ММР", "next_carcass_id": None},
+        {"carcass_id": 6, "value": "1000 ММР", "next_carcass_id": None},
+        {"carcass_id": 6, "value": "2000 ММР", "next_carcass_id": None},
+        {"carcass_id": 6, "value": "3000 ММР", "next_carcass_id": None},
+
+
+        {"carcass_id": 1, "value": "Brawl Stars", "next_carcass_id": 2},
+        {"carcass_id": 2, "value": "Покупка гемов", "next_carcass_id": 3},
+        {"carcass_id": 2, "value": "Боевой пропуск", "next_carcass_id": 5},
+        {"carcass_id": 3, "value": "Supersell ID", "next_carcass_id": 4},
+        {"carcass_id": 3, "value": "Встреча в жизни", "next_carcass_id": 4},
+        {"carcass_id": 3, "value": "Через сторонние", "next_carcass_id": 4},
+        {"carcass_id": 4, "value": "30 Гемов", "next_carcass_id": None},
+        {"carcass_id": 4, "value": "170 Гемов", "next_carcass_id": None},
+        {"carcass_id": 4, "value": "2000 Гемов", "next_carcass_id": None},
+        {"carcass_id": 5, "value": "1 уровень БП", "next_carcass_id": None},
+        {"carcass_id": 5, "value": "5 уровней БП", "next_carcass_id": None},
+        {"carcass_id": 5, "value": "10 уровней БП", "next_carcass_id": None},
+    ]
+
     async with context_get_session() as db_session:
-        categorie_value: models_c.category_values.CategoryValue = (
-            await services_c.categories_values.get_all(db_session=db_session)
-        )
-        if not categorie_value or conf.DROP_TABLES:
-            categorie_value: models_c.category_values.CategoryValue = (
+        values = await services_c.categories_values.get_all(db_session=db_session)
+        if not values or conf.DROP_TABLES:
+            for value_data in values_to_create:
                 await services_c.categories_values.create_value(
                     db_session=db_session,
                     author_id=1,
-                    carcass_id=1,
-                    value=schemas_c.catergories_value.ValueCreate(
-                        value="Brawl Stars",
-                        next_carcass_id=2,
-                    ),
+                    carcass_id=value_data["carcass_id"],
+                    value=schemas_c.ValueCreate(**value_data),
                 )
-            )
-            categorie_value: models_c.category_values.CategoryValue = (
-                await services_c.categories_values.create_value(
-                    db_session=db_session,
-                    author_id=1,
-                    carcass_id=2,
-                    value=schemas_c.catergories_value.ValueCreate(
-                        value="Покупка гемов",
-                        next_carcass_id=3,
-                    ),
-                )
-            )
-            categorie_value: models_c.category_values.CategoryValue = (
-                await services_c.categories_values.create_value(
-                    db_session=db_session,
-                    author_id=1,
-                    carcass_id=3,
-                    value=schemas_c.catergories_value.ValueCreate(
-                        value="Supersell ID",
-                        next_carcass_id=4,
-                    ),
-                )
-            )
-            categorie_value: models_c.category_values.CategoryValue = (
-                await services_c.categories_values.create_value(
-                    db_session=db_session,
-                    author_id=1,
-                    carcass_id=3,
-                    value=schemas_c.catergories_value.ValueCreate(
-                        value="Встреча в жизни",
-                        next_carcass_id=4,
-                    ),
-                )
-            )
-            categorie_value: models_c.category_values.CategoryValue = (
-                await services_c.categories_values.create_value(
-                    db_session=db_session,
-                    author_id=1,
-                    carcass_id=3,
-                    value=schemas_c.catergories_value.ValueCreate(
-                        value="Через сторонние",
-                        next_carcass_id=4,
-                    ),
-                )
-            )
-            categorie_value: models_c.category_values.CategoryValue = (
-                await services_c.categories_values.create_value(
-                    db_session=db_session,
-                    author_id=1,
-                    carcass_id=4,
-                    value=schemas_c.catergories_value.ValueCreate(
-                        value="30 Гемов",
-                        next_carcass_id=None,
-                    ),
-                )
-            )
-            categorie_value: models_c.category_values.CategoryValue = (
-                await services_c.categories_values.create_value(
-                    db_session=db_session,
-                    author_id=1,
-                    carcass_id=4,
-                    value=schemas_c.catergories_value.ValueCreate(
-                        value="170 Гемов",
-                        next_carcass_id=None,
-                    ),
-                )
-            )
-            categorie_value: models_c.category_values.CategoryValue = (
-                await services_c.categories_values.create_value(
-                    db_session=db_session,
-                    author_id=1,
-                    carcass_id=4,
-                    value=schemas_c.catergories_value.ValueCreate(
-                        value="2000 Гемов",
-                        next_carcass_id=None,
-                    ),
-                )
-            )
-            categorie_value: models_c.category_values.CategoryValue = (
-                await services_c.categories_values.create_value(
-                    db_session=db_session,
-                    author_id=1,
-                    carcass_id=2,
-                    value=schemas_c.catergories_value.ValueCreate(
-                        value="Боевой пропуск",
-                        next_carcass_id=5,
-                    ),
-                )
-            )
-            categorie_value: models_c.category_values.CategoryValue = (
-                await services_c.categories_values.create_value(
-                    db_session=db_session,
-                    author_id=1,
-                    carcass_id=5,
-                    value=schemas_c.catergories_value.ValueCreate(
-                        value="1 уровень БП",
-                        next_carcass_id=None,
-                    ),
-                )
-            )
-            categorie_value: models_c.category_values.CategoryValue = (
-                await services_c.categories_values.create_value(
-                    db_session=db_session,
-                    author_id=1,
-                    carcass_id=5,
-                    value=schemas_c.catergories_value.ValueCreate(
-                        value="5 уровеней БП",
-                        next_carcass_id=None,
-                    ),
-                )
-            )
-            categorie_value: models_c.category_values.CategoryValue = (
-                await services_c.categories_values.create_value(
-                    db_session=db_session,
-                    author_id=1,
-                    carcass_id=5,
-                    value=schemas_c.catergories_value.ValueCreate(
-                        value="10 уровеней БП",
-                        next_carcass_id=None,
-                    ),
-                )
-            )
 
 
 async def __init_offers():
+    offers_to_create = [
+        {
+            "name": "BrawStars...",
+            "description": "Brawl..stars",
+            "price": 100,
+            "count": 5,
+            "category_value_ids": [1],
+        },
+        {
+            "name": "BrawStars...",
+            "description": "Brawl..stars",
+            "price": 50,
+            "count": 50,
+            "category_value_ids": [2],
+        },
+        {
+            "name": "BrawStars...",
+            "description": "Brawl..stars",
+            "price": 1000,
+            "count": 20,
+            "category_value_ids": [3],
+        },
+    ]
+
     async with context_get_session() as db_session:
-        offer: models_f.Offer = await sevices_f.get_mini_by_offset_limit(
+        offer = await sevices_f.get_mini_by_offset_limit(
             db_session=db_session, offset=0, limit=10
         )
 
-        if (not offer or conf.DROP_TABLES) and conf.DEBUG:
-            offer: models_f.Offer = await sevices_f.create_offer(
-                db_session=db_session,
-                user_id=1,
-                obj_in=schemas_f.CreateOffer(
-                    name="BrawStars...",
-                    description="Brawl..stars",
-                    price=100,
-                    count=5,
-                    category_value_ids=[1],
-                ),
-            )
-            offer: models_f.Offer = await sevices_f.create_offer(
-                db_session=db_session,
-                user_id=1,
-                obj_in=schemas_f.CreateOffer(
-                    name="BrawStars...",
-                    description="Brawl..stars",
-                    price=50,
-                    count=50,
-                    category_value_ids=[2],
-                ),
-            )
-            offer: models_f.Offer = await sevices_f.create_offer(
-                db_session=db_session,
-                user_id=1,
-                obj_in=schemas_f.CreateOffer(
-                    name="BrawStars...",
-                    description="Brawl..stars",
-                    price=1000,
-                    count=20,
-                    category_value_ids=[3],
-                ),
-            )
+        if not offer or conf.DROP_TABLES:
+            for offer_data in offers_to_create:
+                await sevices_f.create_offer(
+                    db_session=db_session,
+                    user_id=1,
+                    obj_in=schemas_f.CreateOffer(**offer_data),
+                )
 
 
 async def __init_user():
