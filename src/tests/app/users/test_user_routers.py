46c96f8fb04eb3d5375bcf2cadc1e6bf177210d6
Kropka <jugerror@gmail.com>
2024-04-09 03:46:50 +0300

    initial

--- src/tests/app/users/test_user_routers.py
+++ src/tests/app/users/test_user_routers.py
@@ -1,14 +1,34 @@
 import pytest
 from httpx import AsyncClient
-
-pytestmark = pytest.mark.anyio
-
+from sqlalchemy.ext.asyncio import AsyncSession
+from src.core.security import create_jwt_token
+from app.users.models import User
+from app.tokens.schemas import TokenType
+import datetime
+from main import app
 
 # Просто пример выполнения теста
 # В коде таски предполагается использование более продвинутых механизмов
 # К примеру подмена depends, также возможно придётся переработать код в некоторых местах
 # К примеру перенести класс почты в depends через вызов __call__ и return self
 # Всё это нужно будет примерно обсуждать, но я надеюсь от тебя услышать продуманные решения :)
+
+@pytest.mark.asyncio
 async def test_user(async_client: AsyncClient):
     response = await async_client.get("/users/me")
     assert response.status_code == 401
+
+@pytest.mark.asyncio
+async def test_sign_up(async_client: AsyncClient, session: AsyncSession):
+    user_data = {"email": "fagtedsfdsdt@gmail.com", "username": "saftdsaesdsdfdfsdst", "hashed_password": "testpassword"}
+    user = User(**user_data)
+    session.add(user)
+    user.is_verified = True
+    await session.commit()
+
+    token = create_jwt_token(type_= TokenType.access, user_id=user.id, email=user_data["email"], secret="test", expires_delta=datetime.timedelta(minutes=5))
+    async_client.cookies = {"access": token}
+    response = await async_client.get("/users/me")
+    assert response.status_code == 200
+    assert response.json()["email"] == user_data["email"]
+    assert response.json()["username"] == user_data["username"]
--- src/tests/conftest.py
+++ src/tests/conftest.py
@@ -1,15 +1,46 @@
-import pytest
-from httpx import AsyncClient
-
+import pytest_asyncio
+from httpx import ASGITransport, AsyncClient
+from src.core.database import Base
+from src.core.settings.config import DATABASE_TEST_URL
 from main import app
+from sqlalchemy.ext.asyncio import AsyncSession
+from typing import AsyncGenerator
+from sqlalchemy.ext.asyncio import create_async_engine
+from sqlalchemy.orm import sessionmaker
+import asyncio
 
 
-@pytest.fixture(scope="session")
+engine = create_async_engine(DATABASE_TEST_URL, echo=True)
+async_session = sessionmaker(bind=engine, expire_on_commit=False, class_=AsyncSession)
+Base.metadata.bind = engine
+
+@pytest_asyncio.fixture(autouse=True, scope="session")
+async def test_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+
+@pytest_asyncio.fixture(scope="session")
+def event_loop():
+    loop = asyncio.get_event_loop()
+    yield loop
+    loop.close()
+
+
+@pytest_asyncio.fixture(scope="session")
 def anyio_backend():
     return "asyncio"
 
 
-@pytest.fixture(scope="session")
-async def async_client():
-    async with AsyncClient(app=app, base_url="http://testserver") as async_client:
+@pytest_asyncio.fixture(scope="session")
+async def session()-> AsyncGenerator[AsyncSession, None]:
+    async with async_session() as session:
+        yield session
+
+
+@pytest_asyncio.fixture(scope="session")
+async def async_client() -> AsyncGenerator[AsyncClient, None]:
+    async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as async_client:
         yield async_client 
