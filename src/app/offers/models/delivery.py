ceeb846b8cb9c1c0180f81eca14159692d3e5e8a
iyunakov <iyunakov@inbox.ru>
2024-04-25 22:10:55 +0300

    delivery model update

--- src/app/offers/models/delivery.py
+++ src/app/offers/models/delivery.py
@@ -11,9 +11,9 @@ if TYPE_CHECKING:
 # TODO прокинуть связи, маппед и тд
 class Delivery(Base):
     __tablename__ = "delivery"
-    id = Column(Integer, primary_key=True, index=True)
+    
+    id = Column(Integer, primary_key=True)
     offer_id = Column(Integer, ForeignKey('offer.id', ondelete="CASCADE"))
     value = Column(VARCHAR(500))
-    created_at = Column(Integer)
 
     offer: Mapped["Offer"] = relationship(back_populates="deliveries", lazy="noload")
